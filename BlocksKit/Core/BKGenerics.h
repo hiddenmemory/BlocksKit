eb124eeaadc07d9c4614aa87a319d4ed08b85ce0
Chris Ross <chris@hiddenmemory.co.uk>
2015-11-05 15:02:58 -0500

    Testing. One. 2. III.

--- BlocksKit/Core/BKGenerics.h
+++ BlocksKit/Core/BKGenerics.h
@@ -2,6 +2,7 @@
 #define _gsingle(CODE) <CODE>
 #define _gpair(LEFT,RIGHT) <LEFT,RIGHT>
 #else
+#warning Recommend upgrading to Xcode 7
 #define _gsingle(CODE)
 #define _gpair(LEFT,RIGHT)
 #define KeyType id
